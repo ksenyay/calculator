4a406ce7e62e5cb737dfce43b84fa3a7cc9326ce
ksenyay <ksenyausachova@gmail.com>
2024-11-29 14:41:52 +0200

    Add the logic for calculation of percentage

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const clearButton = document.querySelector('.clear');
 const floatButton = document.querySelector('.float');
 const negativeButton = document.querySelector('.negative');
 const operatorButton = document.querySelectorAll('.operator');
-const equalsButton = document.querySelector('.equals')
+const equalsButton = document.querySelector('.equals');
+const percentageButton = document.querySelector('.percentage');
 
 let currentNumber = '';
 let previousNumber = null;
@@ -44,28 +45,12 @@ clearButton.addEventListener('click', () => {
 // Float button logic
 floatButton.addEventListener('click', () => {
     if (input.textContent.includes('.')) {
-        return
+        return;
     } else if (input.textContent == 0) {
         currentNumber = '0.';
         input.textContent = currentNumber; 
     } else {
         currentNumber += '.';
-        input.textContent = currentNumber; 
-    }
-})
-
-// Adding +/- symbols
-negativeButton.addEventListener('click', () => {
-    
-    if (currentNumber.includes('-')) {
-        let deletedMinus = currentNumber.replace('-','');
-        currentNumber = deletedMinus;
-        input.textContent = currentNumber;
-        console.log('includes -')
-    } else if (!currentNumber.includes('-')) {
-        console.log('not includes')
-        let insertedMinus = '-' + currentNumber;
-        currentNumber = insertedMinus;
         input.textContent = currentNumber; 
     }
 })
@@ -99,19 +84,71 @@ equalsButton.addEventListener('click', () => {
 
 // Handle calculation
 function operate(x, y, operator) {
-    x = Number(x);
-    y = Number(y);
+    
+    let xPerc = x;
+    let yPerc = y;
 
-    if (operator == '+') {
-        return x + y;  
-    } else if (operator == '-') {
-        return x - y;
-    } else if (operator == '/') {
-        return x / y;
-    } else if (operator == '*') {
-        return x * y;
+    if (yPerc.includes('%')) {
+        percentageResult = calculatePercentage(xPerc,yPerc,operator);
+        return percentageResult;
+    } else {
+
+        x = Number(x);
+        y = Number(y);
+    
+        if (operator == '+') {
+            return x + y;  
+        } else if (operator == '-') {
+            return x - y;
+        } else if (operator == '/') {
+            return x / y;
+        } else if (operator == '*') {
+            return x * y;
+        }
     }
 }
 
+// Adding +/- symbols
+negativeButton.addEventListener('click', () => {
+    
+    if (currentNumber.includes('-')) {
+        let deletedMinus = currentNumber.replace('-','');
+        currentNumber = deletedMinus;
+        input.textContent = currentNumber;
+    } else if (!currentNumber.includes('-') && currentNumber != 0) {
+        let insertedMinus = '-' + currentNumber;
+        currentNumber = insertedMinus;
+        input.textContent = currentNumber; 
+    }
+})
 
+// Adding % symbol
+percentageButton.addEventListener('click', () => {
+    if (previousNumber != null) {
+        currentNumber = currentNumber + '%';
+        input.textContent = currentNumber;
+    }
+})
 
+// Calculate percentage
+function calculatePercentage(x,y,operator) {
+
+    let stringY = y.replace('%','');
+    let stringX = x;
+
+    let numberX = Number(stringX);
+    let numberY = Number(stringY);
+
+    let percentageY = numberY/100
+    let percentage = percentageY * numberX
+
+    if (operator == '+') {
+        return numberX + percentage;  
+    } else if (operator == '-') {
+        return numberX - percentage;
+    } else if (operator == '/') {
+        return numberX / percentage;
+    } else if (operator == '*') {
+        return numberX * percentage;
+    }
+}
